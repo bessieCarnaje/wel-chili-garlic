8dcddaa87b62bb3fa0bf89876e8753b2cc4fb733
bessie <bmec.622@gmail.com>
2023-05-02 10:46:15 +0800

    image

--- src/pages/Order/ImagePanel.js
+++ src/pages/Order/ImagePanel.js
@@ -1,5 +1,29 @@
 import React from "react";
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import chili1 from "../../images/chili1.jpg";
+import chili2 from "../../images/chili2.jpg";
+import chili3 from "../../images/chili3.jpg";
 
 export default function ImagePanel() {
-  return <div className="chili-album"></div>;
+  const images = [chili1, chili2, chili3];
+
+  return (
+    <div className="chili-album">
+      <Slider
+        dots={true}
+        infinite={true}
+        speed={500}
+        slidesToShow={1}
+        slidesToScroll={1}
+      >
+        {images.map((image, index) => (
+          <div key={index} className="slider">
+            <img src={image} alt={`Image ${index}`} className="slider-image" />
+          </div>
+        ))}
+      </Slider>
+    </div>
+  );
 }
